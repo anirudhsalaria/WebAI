c52b4c6d89a022ba513b70fa3b831167ef858a77
Anirudh Salaria <salariaanirudh@gmail.com>
2023-08-21 20:27:13 +0530

    update

---
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from '@/components/ui/button'
+import React from 'react'
+
+const DashboardPage = () => {
+  return (
+    <div>
+      <Button variant={'destructive'} size={'lg'}>Click me </Button>
+    </div>
+  )
+}
+
+export default DashboardPage
