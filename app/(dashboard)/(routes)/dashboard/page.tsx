b557f634a88c96b5334279fa81091485218bdc34
Anirudh Salaria <salariaanirudh@gmail.com>
2023-08-23 11:41:11 +0530

    Update page.tsx

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -7,12 +7,40 @@ const tools = [
     color: "text-violet-500",
     bgColor: "bg-violet-500/10",
     href: "/converation"
-  }
+  },
+  {
+    label: "Music Generation",
+    icon: Music,
+    color: "text-emerald-500",
+    bgColor: "bg-emerald-500/10",
+    href: "/music"
+  },
+  {
+    label: "Image Generation",
+    icon: Image,
+    color: "text-pink-700",
+    bgColor: "bg-pink-700/10",
+    href: "/image"
+  },
+  {
+    label: "Video Generation",
+    icon: Video,
+    color: "text-orange-700",
+    bgColor: "bg-orange-700/10",
+    href: "/video"
+  },
+  {
+    label: "Code Generation",
+    icon: Code,
+    color: "text-green-700",
+    bgColor: "bg-green-700/10",
+    href: "/code"
+  },
 ]
 
 import { Card } from '@/components/ui/card';
 import { cn } from '@/lib/utils';
-import { ArrowRight, MessageSquare } from 'lucide-react';
+import { ArrowRight, Code, Image, MessageSquare, Music, Video } from 'lucide-react';
 import React from 'react'
 
 const DashboardPage = () => {
