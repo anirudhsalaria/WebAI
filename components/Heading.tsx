f816bd250eea392bd6eac3c49ffc41e250fc44b3
Anirudh Salaria <salariaanirudh@gmail.com>
2023-08-24 21:53:15 +0530

    update

--- components/Heading.tsx
+++ components/Heading.tsx
@@ -1,10 +1,11 @@
+import { cn } from '@/lib/utils';
 import { LucideIcon } from 'lucide-react';
 import React from 'react'
 
 interface HeadingProps {
-    title:string;
-    description:string;
-    icon:LucideIcon;
+    title: string;
+    description: string;
+    icon: LucideIcon;
     iconColor?: string;
     bgColor?: string;
 }
@@ -12,16 +13,29 @@ interface HeadingProps {
 const Heading = ({
     title,
     description,
-    icon,
+    icon: Icon,
     iconColor,
     bgColor
 }: HeadingProps
 ) => {
-  return (
-    <div>
-      Heading Component
-    </div>
-  )
+    return (
+        <>
+            <div className='px-4 lg:px-8 flex items-center gap-x-3 mb-8'>
+                <div className={cn("p-2 w-fit rounded-md", bgColor)}>
+                    <Icon className={cn("w-10 h-10", iconColor)} />
+                </div>
+
+                <div>
+                    <h2 className='text-3xl font-bold'>
+                        {title}
+                    </h2>
+                    <p className='text-sm text-muted-foreground'>
+                        {description}
+                    </p>
+                </div>
+            </div>
+        </>
+    )
 }
 
 export default Heading
